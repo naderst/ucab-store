1a60070909509dce4b63e9c33e6602d8efbc9396
Nader Abu Fakhr <naderst@gmail.com>
2014-10-28 23:35:00 -0430

    Agregada configuración de BD a gitignore

--- app/config/database.config.php
+++ app/config/database.config.php
@@ -55,7 +55,7 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'ucab-blog',
+			'database'  => 'ucab-store',
 			'username'  => 'root',
 			'password'  => '',
 			'charset'   => 'utf8',
