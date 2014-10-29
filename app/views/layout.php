92b95353a7b4974bbe4d9fe473c26e7d9f0ce293
Nader Abu Fakhr <naderst@gmail.com>
2014-10-28 23:22:38 -0430

    Añadido AngularJS al layout

---
+++ app/views/layout.php
@@ -0,0 +1,13 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>UCAB - Store</title>
+</head>
+
+<body>
+    <p>Layout de la aplicación</p>
+    <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.2.26/angular.min.js"></script>
+</body>
+</html>
