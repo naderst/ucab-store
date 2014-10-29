fae58a88e823b9d4405fd0075731ef0e186b967a
Nader Abu Fakhr <naderst@gmail.com>
2014-10-29 16:33:23 -0430

    Creados seeders para la BD

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -11,7 +11,73 @@ class DatabaseSeeder extends Seeder {
 	{
 		Eloquent::unguard();
 
+        $this->call('ZapatosTableSeeder');
+        
 		// $this->call('UserTableSeeder');
 	}
 
+}
+
+class ZapatosTableSeeder extends Seeder {
+
+    public function run()
+    {
+        DB::table('tallas')->delete();
+        DB::table('zapatos')->delete();
+        
+        /*
+            Nike Lebron 12 iD
+        */
+        
+        $id = Zapato::create(array(
+            'descripcion' => 'Zapato para Basketball',
+            'modelo' => 'Lebron 12 iD',
+            'precio' => 12250.00,
+            'color' => 'Negro',
+            'foto' => 'Nike-Lebron12iD.jpg'
+        ))->id;
+        
+        Talla::create(array(
+            'talla' => 9,
+            'zapato_id' => $id
+        ));
+        
+        Talla::create(array(
+            'talla' => 10,
+            'zapato_id' => $id
+        ));
+        
+        Talla::create(array(
+            'talla' => 11,
+            'zapato_id' => $id
+        ));
+        
+        /*
+            Adidas Messi F10
+        */
+        
+        $id = Zapato::create(array(
+            'descripcion' => 'Zapato para fútbol',
+            'modelo' => 'Messi F10',
+            'precio' => 3000.00,
+            'color' => 'Blanco',
+            'foto' => 'Adidas-Messi-F10.jpg'
+        ))->id;
+        
+        Talla::create(array(
+            'talla' => 6,
+            'zapato_id' => $id
+        ));
+        
+        Talla::create(array(
+            'talla' => 7,
+            'zapato_id' => $id
+        ));
+        
+        Talla::create(array(
+            'talla' => 8,
+            'zapato_id' => $id
+        ));
+    }
+    
 }
